d82552b3345f488d20623d89df8ccf9d9f754f63
prinzprogrammer <princeprogrammer73@gmail.com>
2023-03-02 21:28:27 +0800

    fix comments and renamed variable

--- script.js
+++ script.js
@@ -43,40 +43,55 @@ clearBtn.addEventListener("click", clear);
 
 //You should be storing the ‘display value’ in a variable somewhere for use in the next step
 let num1 = 0;
+let num2Holder = 0;
 let num2 = 0;
 let operator;
 let result = 0;
 
 function populateDisplay() {
-  //Create the functions that populate the display when you click the number buttons.
   for (let index = 0; index < numList.length; index++) {
     numList[index].addEventListener("click", function () {
+      //populate the display when you click the number buttons
       input.innerHTML += numList[index].value;
     });
   }
-  let secondInput = 0;
+
   for (let index = 0; index < operatorList.length; index++) {
     operatorList[index].addEventListener("click", function () {
-      num1 = Number(input.innerHTML); //store the first number that is input into the calculator when a user presses an operator
+      //store the first number that is input into the calculator when a user presses an operator
+      num1 = Number(input.innerHTML);
       console.log("num 1:", num1);
+
       operator = operatorList[index].value;
+      console.log("operator :", operator);
+      //populate the display when you click the number buttons
       input.innerHTML += ` ${operator} `;
-      secondInput = 0;
+
+      num2Holder = 0;
+
       for (let index = 0; index < numList.length; index++) {
         numList[index].addEventListener("click", function () {
-          secondInput += numList[index].value; //store input.innerHTML into another variable and store numList[index].value
+          num2Holder += numList[index].value; //store input.innerHTML into another variable and store numList[index].value
+          console.log("num2 Holder:", num2Holder);
         });
       }
     });
   }
   equalBtn.addEventListener("click", function () {
-    num2 = Number(secondInput); //Store second number after pressing equals button
-    console.log("Second Input:", secondInput);
+    console.log("num2 Holder:", num2Holder);
+    num2 = Number(num2Holder); //Store second number after pressing equals button
     console.log("num 2:", num2);
     result = operate(operator, num1, num2);
     console.log("Result:", result);
     input.innerHTML = result;
-    console.log("/* ----- */ ");
+    num1 = 0;
+    num2 = 0;
+    operator;
   });
 }
 populateDisplay();
+
+function storeNum2() {
+  num2Holder += numList[index].value; //store input.innerHTML into another variable and store numList[index].value
+  console.log("num2 Holder:", num2Holder);
+}
